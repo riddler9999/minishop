d0ce899c128bb1ab97333b10278a9f3cc08fe771
riddler9999 <82827741+riddler9999@users.noreply.github.com>
2026-09-21 11:50:24 +0630

    chore: use static robots file only

--- api/robots.ts
+++
@@ -1,5 +0,0 @@
-export default function handler(_req: any, res: any) {
-  res.setHeader('Content-Type', 'text/plain; charset=utf-8');
-  res.setHeader('Cache-Control', 'public, max-age=86400');
-  res.status(200).send('User-agent: *\nDisallow: /api/\n');
-}
