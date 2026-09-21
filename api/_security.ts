c2c589247bf1967311b7152d847c3071a1999e32
riddler9999 <82827741+riddler9999@users.noreply.github.com>
2026-09-21 11:51:03 +0630

    security: add common API response headers

---
+++ api/_security.ts
@@ -0,0 +1,5 @@
+export function secure(res: any) {
+  res.setHeader('X-Content-Type-Options', 'nosniff');
+  res.setHeader('Referrer-Policy', 'no-referrer');
+  res.setHeader('X-Frame-Options', 'DENY');
+}
